d4615cd7c3abc9dd15e1264fff9845372bcec638
kk25114 <kk25114@gmail.com>
2023-09-27 20:07:21 +0800

    更新 Chat.tsx
    更新店铺

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -377,7 +377,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
             <div>
               {t("If you don't have an OpenAI API key, you can get one here: ")}
               <a
-                href="https://kk25114.shop/buy/26"
+                href="https://kkkk25114.shop/buy/26"
                 target="_blank"
                 rel="noreferrer"
                 className="text-blue-500 hover:underline"
